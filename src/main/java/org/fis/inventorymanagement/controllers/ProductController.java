d182ef73f9724e37bf3a34cbe7dac30865bebe36
samuelescYT <menudesamy@gmail.com>
2024-11-05 10:13:35 -0500

    Controller de producto implementado

--- src/main/java/org/fis/inventorymanagement/controllers/ProductController.java
+++ src/main/java/org/fis/inventorymanagement/controllers/ProductController.java
@@ -1,10 +1,13 @@
 package org.fis.inventorymanagement.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.fis.inventorymanagement.models.Product;
+import org.fis.inventorymanagement.models.Sale;
 import org.fis.inventorymanagement.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -16,5 +19,30 @@ public class ProductController {
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProduct() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id) {
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
+        return productService.updateProduct(id, updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable int id) {
+        productService.deleteProduct(id);
     }
 }
